c8b89b64b7bed4eecea7947093c2d20b69a33188
Ayesha <asiddiqua004@gmail.com>
2020-10-26 00:33:21 -0700

    Add files

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import asyncio
+import uuid
+import time
+import sys
+from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device import Message
+
+async def main():
+
+    conn_str = "HostName=5G-IoT-System-For-Emergency-Responders.azure-devices.net;DeviceId=application_device1;SharedAccessKey=x84oYfc8Wm4lL7nfMzNm87X7YmFbC+TtHX4ny+bV8ck="
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a few messages
+    count = 0
+    while count < 5:
+        print("Sending message to Azure IoT...")
+        msg = Message("Environment sensor")
+        msg.message_id = uuid.uuid4()
+        msg.custom_properties["Temperature"] = "45"
+        await device_client.send_message(msg)
+        print("Message successfully sent:")
+        print(msg.custom_properties)
+        count = count + 1
+        time.sleep(10)
+
+    # finally, disconnect
+    await device_client.disconnect()
+    sys.exit(0)
+
+if __name__ == "__main__":
+    asyncio.run(main())
